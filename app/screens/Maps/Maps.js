ac1ff02f5d55fca22f6162a00a1f14b9378b0ecd
leiram8 <64088844+leiram8@users.noreply.github.com>
2021-11-24 18:16:54 -0300

    Actualización mapa
    Agregue marcador y baje la información para que se vea bien la posición en el mapa.

--- app/screens/Maps/Maps.js
+++ app/screens/Maps/Maps.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import {
   View,
   StyleSheet,
@@ -9,54 +9,22 @@ import {
 } from 'react-native';
 import * as SQLite from 'expo-sqlite';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-import axios from 'axios';
 
 const db = SQLite.openDatabase('city_db.db');
 
 const Maps = ({ route, navigation }) => {
 
-  const { ciudad, provincia, pais } = route.params;
-  const [coordCity, setCoordCity] = useState({});
+  const { cityLon, cityLat, ciudad, provincia, pais } = route.params;
 
-  //Obtenemos las coordenadas de la API de Openweather para pasarlas al mapa
-  function getCoord() {
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=128cbb6d697b9515f235f503e5961922&units=metric&lang=esp`)
-      .then(response => {
-        const info = response.data;
-        setCoordCity({
-          ciudad: info.name,
-          pais: info.sys.country,
-          lon: info.coord.lon,
-          lat: info.coord.lat
-        });
-        //console.log('weather', info);
-        console.log('Longitud:', info.coord.lon);
-        console.log('Latitud:', info.coord.lat);
-      }).catch((error) => {
-        Alert.alert(
-          'Advertencia', 'Ciudad no encontrada, revise los datos ingresados',
-          [
-            {
-              text: 'Ok',
-              onPress: () => navigation.navigate('Add'),
-            },
-          ],
-          { cancelable: false },
-        );
-      })
-  }
-
-  useEffect(() => {
-    getCoord()
-  }, []);
-
-  //Definimos la localizacion con los datos obtenidos de la consulta a la API OpenWeather
+  const lon = parseFloat(cityLon);
+  const lat = parseFloat(cityLat);
   const location = {
-    latitude: coordCity.lat,
-    longitude: coordCity.lon,
+    latitude: lat,
+    longitude: lon,
     latitudeDelta: 0.5,
-    longitudeDelta: 0.5
-  }
+    longitudeDelta: 0.5,
+  };
+
 
   //Funcion para registrar Ciudad en la Base de Datos si la localizacion es la correcta
   function registerCity() {
@@ -86,39 +54,35 @@ const Maps = ({ route, navigation }) => {
   }
 
   return (
-
     <View style={styles.container}>
       <MapView
         provider={PROVIDER_GOOGLE}
         style={styles.mapStyle}
-        mapType='hybrid'
+        mapType='standard'
         region={location}
-      ></MapView>
-      <View style={styles.btn_row}>
-        <Button
-          title="Cancelar"
-          color="#CD5C5C"
-          onPress={
-            () => {
-              navigation.navigate('Add');
-            }
-          }
+      >
+        <MapView.Marker
+          coordinate={{ latitude: location.latitude, longitude: location.longitude }}
         />
-        <Button
-          title="Confirmar"
-          color="#CD5C5C"
-
-          onPress={
-            () => {
-              registerCity();
-            }
-          }
-        />
-      </View>
-      <View style={styles.txt_map}>
-        <Text>Ciudad: {route.params.ciudad} </Text>
-        <Text>Latitud: {coordCity.lat} </Text>
-        <Text>Longitud: {coordCity.lon} </Text>
+      </MapView>
+      <View style={styles.bottomView}>
+        <View style={styles.txt_map}>
+          <Text>Ciudad: {ciudad} </Text>
+          <Text>Longitud: {lon}</Text>
+          <Text>Latitud: {lat}</Text>
+        </View>
+        <View style={styles.btn_row}>
+          <Button
+            title="Cancelar"
+            color="#CD5C5C"
+            onPress={() => navigation.navigate("Add")}
+          />
+          <Button
+            title="Confirmar"
+            color="#CD5C5C"
+            onPress={() => registerCity()}
+          />
+        </View>
       </View>
     </View>
   );
@@ -142,8 +106,6 @@ const styles = StyleSheet.create({
     alignContent: 'center',
   },
   txt_map: {
-    height: 80,
-    width: '60%',
     margin: 10,
     backgroundColor: 'white',
     borderColor: 'black',
@@ -151,6 +113,14 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  bottomView: {
+    height: 80,
+    width: "60%",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    bottom: 30,
   },
 });
 
